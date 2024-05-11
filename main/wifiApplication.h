c890a73fa48ca1cac3b35f450f4a7315e0867408
Phong Nguyen <118428350+urboob21@users.noreply.github.com>
2024-05-11 15:31:20 +0700

    rename wifi host

--- main/wifiApplication.h
+++ main/wifiApplication.h
@@ -14,34 +14,34 @@
 typedef void (*wifi_connected_event_callback_t)(void);
 
 // WiFi application settings
-#define WIFI_AP_SSID "ESP32_AP_CLIENT2"			 // AP name
-#define WIFI_AP_PASSWORD "password"		 // AP password
-#define WIFI_AP_CHANNEL 1				 // AP channel
-#define WIFI_AP_SSID_HIDDEN 0			 // AP visibility
-#define WIFI_AP_MAX_CONNECTIONS 5		 // AP max clients
-#define WIFI_AP_BEACON_INTERVAL 100		 // AP beacon: 100 milliseconds recommended
-#define WIFI_AP_IP "192.168.0.1"		 // AP default IP
-#define WIFI_AP_GATEWAY "192.168.0.1"	 // AP default Gateway (should be the same as the IP)
-#define WIFI_AP_NETMASK "255.255.255.0"	 // AP netmask
-#define WIFI_AP_BANDWIDTH WIFI_BW_HT20	 // AP bandwidth 20 MHz (40 MHz is the other option)
-#define WIFI_STA_POWER_SAVE WIFI_PS_NONE // Power save not used
-#define MAX_SSID_LENGTH 32				 // IEEE standard maximum
-#define MAX_PASSWORD_LENGTH 64			 // IEEE standard maximum
-#define MAX_CONNECTION_RETRIES 3		 // Retry number on disconnect
+#define WIFI_AP_SSID "THING-DEVICE-0000-ESP32" // AP name
+#define WIFI_AP_PASSWORD "password"			   // AP password
+#define WIFI_AP_CHANNEL 1					   // AP channel
+#define WIFI_AP_SSID_HIDDEN 0				   // AP visibility
+#define WIFI_AP_MAX_CONNECTIONS 5			   // AP max clients
+#define WIFI_AP_BEACON_INTERVAL 100			   // AP beacon: 100 milliseconds recommended
+#define WIFI_AP_IP "192.168.0.1"			   // AP default IP
+#define WIFI_AP_GATEWAY "192.168.0.1"		   // AP default Gateway (should be the same as the IP)
+#define WIFI_AP_NETMASK "255.255.255.0"		   // AP netmask
+#define WIFI_AP_BANDWIDTH WIFI_BW_HT20		   // AP bandwidth 20 MHz (40 MHz is the other option)
+#define WIFI_STA_POWER_SAVE WIFI_PS_NONE	   // Power save not used
+#define MAX_SSID_LENGTH 32					   // IEEE standard maximum
+#define MAX_PASSWORD_LENGTH 64				   // IEEE standard maximum
+#define MAX_CONNECTION_RETRIES 3			   // Retry number on disconnect
 
 extern esp_netif_t *esp_netif_sta;
 
 /**
  * Define msgId for message
-*/
+ */
 typedef enum
 {
-	WIFI_APP_MSG_START_HTTP_SERVER = 0,			// default start
+	WIFI_APP_MSG_START_HTTP_SERVER = 0,		  // default start
 	WIFI_APP_MSG_CONNECTING_FROM_HTTP_SERVER, // when press Btn Connect on web-config-local
-	WIFI_APP_MSG_STA_CONNECTED_GOT_IP, 		 // when connect with station
-	WIFI_APP_MSG_STA_DISCONNECTED,			
-	WIFI_APP_MSG_LOAD_SAVED_CREDENTIALS,	// when request load 
-	WIFI_APP_MSG_USER_REQUESTED_STA_DISCONNECT 	// when press Btn Disconnect 
+	WIFI_APP_MSG_STA_CONNECTED_GOT_IP,		  // when connect with station
+	WIFI_APP_MSG_STA_DISCONNECTED,
+	WIFI_APP_MSG_LOAD_SAVED_CREDENTIALS,	   // when request load
+	WIFI_APP_MSG_USER_REQUESTED_STA_DISCONNECT // when press Btn Disconnect
 } wifi_app_message_t;
 
 /**
